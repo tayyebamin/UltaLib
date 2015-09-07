00a590cb1e000fd8caf5d8de0e3a93328807f42d
Daniel Nichols <dando18studios@gmail.com>
2015-09-07 09:33:58 -0400

    removed sqrt() method

--- src/lib/Maths.java
+++ src/lib/Maths.java
@@ -10,66 +10,17 @@ import java.util.List;
 import java.util.Locale;
 
 public class Maths {
-	
-	private static final double TWO_54 = 0x40000000000000L;
-	
+		
 	private Maths(){}
-	
-	public static double sqrt(double x){
-	     if (x < 0)
-	       return Double.NaN;
-	     if (x == 0 || ! (x < Double.POSITIVE_INFINITY))
-	       return x;
-	 
-	     // Normalize x.
-	     long bits = Double.doubleToLongBits(x);
-	     int exp = (int) (bits >> 52);
-	     if (exp == 0) // Subnormal x.
-	      {
-	         x *= TWO_54;
-	         bits = Double.doubleToLongBits(x);
-	         exp = (int) (bits >> 52) - 54;
-	       }
-	     exp -= 1023; // Unbias exponent.
-	     bits = (bits & 0x000fffffffffffffL) | 0x0010000000000000L;
-	     if ((exp & 1) == 1) // Odd exp, double x to make it even.
-	       bits <<= 1;
-	     exp >>= 1;
-	 
-	     // Generate sqrt(x) bit by bit.
-	     bits <<= 1;
-	     long q = 0;
-	     long s = 0;
-	     long r = 0x0020000000000000L; // Move r right to left.
-	     while (r != 0)
-	       {
-	         long t = s + r;
-	                  if (t <= bits)
-	           {
-	             s = t + r;
-	             bits -= t;
-	             q += r;
-	           }
-	         bits <<= 1;
-	         r >>= 1;
-	       }
-	 
-	     // Use floating add to round correctly.
-	     if (bits != 0)
-	    	        q += q & 1;
-	     return Double.longBitsToDouble((q >> 1) + ((exp + 1022L) << 52));
-	   }
 	
 	public static int bitwiseMultiply(int n1, int n2) {
         int a = n1, b = n2, result=0;
-        while (b != 0) // Iterate the loop till b==0
-        {
-            if ((b & 01) != 0) // Logical ANDing of the value of b with 01
-            {
-                result = result + a; // Update the result with the new value of a.
+        while (b != 0) {
+            if ((b & 01) != 0) {
+                result = result + a; 
             }
-            a <<= 1;              // Left shifting the value contained in 'a' by 1.
-            b >>>= 1;             // Right shifting the value contained in 'b' by 1.
+            a <<= 1;              
+            b >>>= 1;             
         }
         return result;
 	}
